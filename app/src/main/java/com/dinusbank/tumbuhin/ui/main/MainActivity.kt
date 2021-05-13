d2b7120f2f2a7ef5f4d908d8e3d00a3d3bf09be8
rezaharisz <rezavonstrucker@gmail.com>
2021-05-13 16:31:47 +0700

    fix exit function

--- app/src/main/java/com/dinusbank/tumbuhin/ui/main/MainActivity.kt
+++ app/src/main/java/com/dinusbank/tumbuhin/ui/main/MainActivity.kt
@@ -2,12 +2,17 @@ package com.dinusbank.tumbuhin.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dinusbank.tumbuhin.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var doubleBackExit = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +22,19 @@ class MainActivity : AppCompatActivity() {
 
         navView.setupWithNavController(navController)
         navView.itemIconTintList = null
+
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackExit){
+            finishAffinity()
+            return
+        }
+
+        this.doubleBackExit = true
+        Toast.makeText(this, "Klik dua kali untuk exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(
+            { doubleBackExit = false }, 2000)
     }
 }
