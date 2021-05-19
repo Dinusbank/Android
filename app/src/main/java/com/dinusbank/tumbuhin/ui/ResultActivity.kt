ac60746ef5b822f712fcc204949948a010222ab6
rezaharisz <rezavonstrucker@gmail.com>
2021-05-19 12:11:48 +0700

    add gallery function, back button function, and few adjustment

--- app/src/main/java/com/dinusbank/tumbuhin/ui/ResultActivity.kt
+++ app/src/main/java/com/dinusbank/tumbuhin/ui/ResultActivity.kt
@@ -1,12 +1,27 @@
 package com.dinusbank.tumbuhin.ui
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dinusbank.tumbuhin.R
+import com.dinusbank.tumbuhin.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle: Bundle? = intent.extras
+        if (bundle != null){
+            val setImage: Uri = Uri.parse(bundle.getString("imageId"))
+            binding.ivLeafes.setImageURI(setImage)
+        }
+
+        binding.btnBack.setOnClickListener {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/dinusbank/tumbuhin/ui/main/HomeFragment.kt
+++ app/src/main/java/com/dinusbank/tumbuhin/ui/main/HomeFragment.kt
@@ -1,17 +1,65 @@
 package com.dinusbank.tumbuhin.ui.main
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager.*
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dinusbank.tumbuhin.R
+import androidx.core.app.ActivityCompat
+import com.dinusbank.tumbuhin.databinding.FragmentHomeBinding
+import com.dinusbank.tumbuhin.ui.ResultActivity
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private lateinit var binding: FragmentHomeBinding
+    private var imageUri: Uri? = null
+
+    companion object{
+        private const val PICK_IMAGE_GALLERY = 200
+        private const val PERMISSION_CODE = 100
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnUpload.setOnClickListener {
+            if(context?.let { it1 -> ActivityCompat.checkSelfPermission(it1, Manifest.permission.READ_EXTERNAL_STORAGE) }
+                == PERMISSION_DENIED){
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permission, PERMISSION_CODE)
+            } else{
+                pickImageFromGallery()
+            }
+        }
+    }
+
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_GALLERY){
+            imageUri = data?.data
+
+            val intent = Intent(activity, ResultActivity::class.java)
+            intent.putExtra("imageId", imageUri.toString())
+
+            startActivity(intent)
+        }
     }
 
 }
